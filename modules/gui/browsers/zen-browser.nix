ab3de2595f1f6a06db11eb40d0994eaf474cc6da
Marviel Billante <marvielb@gmail.com>
2026-06-12 18:36:28 +0800

    refactor(gui/browsers): move Zen Browser from programs/ to gui/browsers/
    Renames catalog key from programs_zen-browser to gui_browsers_zen-browser
    to match directory structure convention.

--- modules/gui/browsers/zen-browser.nix
+++ modules/gui/browsers/zen-browser.nix
@@ -1,5 +1,5 @@
 { inputs, ... }: {
-  flake.modules.nixos.programs_zen-browser = { pkgs, ... }: {
+  flake.modules.nixos.gui_browsers_zen-browser = { pkgs, ... }: {
     environment.systemPackages = [
       inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
