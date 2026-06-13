45769de79db0b210ef436e61135f86c3056c9960
Marviel Billante <marvielb@gmail.com>
2026-06-13 19:17:09 +0800

    fix(programs/lazyvim): use stylix-managed base16 colorscheme
    Replace the manual base16 config approach with stylix-managed theming.
    LazyVims default tokyonight colorscheme is disabled via opts override
    so stylix controls the colorscheme exclusively. Add transparent
    background support for main, signcolumn, and numberline panes. Update
    plugin name to reflect the upstream rename to nvim-mini/mini.base16.

--- modules/home/programs/lazyvim.nix
+++ modules/home/programs/lazyvim.nix
@@ -1,15 +1,29 @@
 { inputs, ... }: {
   flake.modules.homeManager.programs_lazyvim = { ... }: {
     imports = [ inputs.lazyvim.homeManagerModules.default ];
+
+    stylix.targets.neovim.transparentBackground = {
+      main = true;
+      signColumn = true;
+      numberLine = true;
+    };
+
     programs.lazyvim = {
       enable = true;
       plugins = {
         "mini-base16" = ''
           return {
-            "echasnovski/mini.base16",
-            name = "mini.base16",
+            "nvim-mini/mini.base16",
             lazy = false,
             priority = 1000,
+          }
+        '';
+        "disable-lazyvim-colorscheme" = ''
+          return {
+            "LazyVim/LazyVim",
+            opts = {
+              colorscheme = function() end,
+            },
           }
         '';
       };
